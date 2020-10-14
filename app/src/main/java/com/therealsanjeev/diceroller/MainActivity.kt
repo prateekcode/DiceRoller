15ed7eaa04bb7037c1323b549a771ad6ad94bf95
Prateek <codes.prateek@gmail.com>
2020-10-14 22:56:36 +0530

    Updated MainActivity File

--- app/src/main/java/com/therealsanjeev/diceroller/MainActivity.kt
+++ app/src/main/java/com/therealsanjeev/diceroller/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        //I'm editing your file
     }
 }
